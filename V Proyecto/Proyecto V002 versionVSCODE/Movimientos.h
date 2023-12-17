2895960117048e5c00ef5f0ef443fdbee4d738b6
Yeshua <yeshuachiliquinga@gmail.com>
2023-12-17 16:11:37 -0500

    MinGW version update V002

---
+++ V Proyecto/Proyecto V002 versionVSCODE/Movimientos.h
@@ -0,0 +1,11 @@
+#ifndef MOVIMIENTOS_H_INCLUDED
+#define MOVIMIENTOS_H_INCLUDED
+#include "Amortizacion.h" // Incluye la clase Amortizacion
+class Movimiento{
+public:
+    void crear_Credito();
+
+
+};
+
+#endif // MOVIMIENTOS_H_INCLUDED
---
+++ V Proyecto/Proyecto V002 versionVSCODE/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "menu.h"
+#include "Amortizacion.h" // Incluye la clase Amortizacion
+#include "Movimientos.h"  // Incluye la clase Movimiento
+
+using namespace std;
+
+int main()
+{
+    Menus menu;
+    menu.Menu_Principal();
+    return 0;
+}
