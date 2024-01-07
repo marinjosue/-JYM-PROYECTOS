b486bef66de30fe9a83bba5fe24d35184f067e9f
marinjosue <josuisaac2002@gmail.com>
2024-01-07 03:11:31 -0500

    Update validacion de la cedula, registro de personas e imprime personas de mi lista

---
+++ ORDENAMIENTO/2.Ordenamiento V0002/Persona.h
@@ -0,0 +1,41 @@
+/***********************************************************************
+ * Module:  Persona.h
+ * Author:  Usuario
+ * Modified: domingo, 7 de enero de 2024 0:07:18
+ * Purpose: Declaration of the class Persona
+ ***********************************************************************/
+
+#if !defined(__Persona_Persona_h)
+#define __Persona_Persona_h
+#include <string>
+
+class Persona
+{
+public:
+   Persona();
+   Persona(std::string Nombre, std::string Apellido, std::string Cedula, std::string Id);
+   ~Persona();
+   std::string getNombre(void);
+   void setNombre(std::string newNombre);
+   std::string getApellido(void);
+   void setApellido(std::string newApellido);
+   std::string getCedula(void);
+   void setCedula(std::string newCedula);
+   std::string getId(void);
+   void setId(std::string newId);
+   void ingresarPersona();
+   void mostrarRegistroPersona();
+
+
+protected:
+private:
+   std::string Nombre;
+   std::string Apellido;
+   std::string Cedula;
+   std::string Id;
+
+
+
+};
+
+#endif
