f147dbf2910c08487bc623c40e33f32eede2e552
marinjosue <josuisaac2002@gmail.com>
2024-01-09 22:13:14 -0500

    Update duncion ordenar con parametro

---
+++ DEBERES GRUPALES/ORDENAMIENTO/12.Ordenamiento V012/ListaDoble.h
@@ -0,0 +1,57 @@
+/*************************
+ UFA - ESPE
+ * Module:  ListaDoble.h
+ * Author:  Chiliquinga Yeshua, Marin Alquinga,Salcedo Micaela
+ * Modified: domingo, 17 de diciembre de 2023
+ * Purpose: Declaracion del encabezado ListaDoble
+ ***********************************************************************/
+#ifndef LISTADOBLE_H_INCLUDED
+#define LISTADOBLE_H_INCLUDED
+
+#include "Nodo.cpp"
+
+
+
+class Persona;
+
+template <typename T>
+class ListaDoble
+{
+public:
+    ListaDoble<T>();
+    bool esta_vacia();
+    void insertarAlInicio(Persona& persona);
+    void insertarAlFinal(Persona& persona);
+    void eliminarAlInicio();
+    void eliminarAlFinal();
+    void mostrarLista();
+    bool buscarCedulaExistente(const std::string& cedula) const;
+    void orden_seleccion(bool (*criterio)(T, T));
+
+protected:
+private:
+    Nodo<T>* cabeza;
+    Nodo<T>* cola;
+    Nodo<T>* siguiente;
+    Nodo<T>* anterior;
+};
+
+template <typename T>
+bool ListaDoble<T>::buscarCedulaExistente(const std::string& cedula) const  {
+    Nodo<T>* nodo_actual = cabeza;
+
+    while (nodo_actual != nullptr) {
+        Persona* persona_actual = nodo_actual->get_valor();
+
+        if (persona_actual->getCedula() == cedula) {
+            return false;
+        }
+
+        nodo_actual = nodo_actual->get_siguiente();
+    }
+
+
+    return true;
+}
+
+#endif // LISTADOBLE_H_INCLUDED
---
+++ DEBERES GRUPALES/ORDENAMIENTO/12.Ordenamiento V012/main.cpp
@@ -0,0 +1,21 @@
+/***********************************************************************
+ * Module:  main.cpp
+ * Author:  Josue Marin
+ * Modified: viernes, 1 de diciembre de 2023 12:05:38 a. m.
+ * Purpose: Declaration of the class main
+ ***********************************************************************/
+
+#include <iostream>
+#include "Nodo.h"
+#include "menu.h"
+#include "ListaDoble.h"
+
+ ListaDoble<Persona*> listaPersonas;
+
+int main()
+{
+
+    Menus menu;
+    menu.Menu_Principal();
+    return 0;
+}
