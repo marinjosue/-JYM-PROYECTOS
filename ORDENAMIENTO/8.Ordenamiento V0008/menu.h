ba1a275d4b3394c73b33f547019c243d8e793c86
marinjosue <josuisaac2002@gmail.com>
2024-01-09 20:11:49 -0500

    Update funcion Para ordener y menu

---
+++ ORDENAMIENTO/8.Ordenamiento V0008/menu.h
@@ -0,0 +1,25 @@
+/*************************
+    UFA - ESPE
+ * Module:  Menus.h
+ * Author:  Chiliquinga Yeshua, Marin Alquinga, Salcedo Micaela
+ * Modified: domingo, 17 de diciembre de 2023
+ * Purpose: Declaracion del encabezado Menus
+ ***********************************************************************/
+#ifndef MENU_H
+#define MENU_H
+
+#include "ListaDoble.h"  // Agrega la inclusión del archivo de encabezado de ListaDoble
+#include "Persona.h"    // Agrega la inclusión del archivo de encabezado de Persona
+
+class Menus {
+public:
+    void gotoxy(int x, int y);
+    void color(int foreground, int background);
+    void menuSeleccion(const char* titulo,const char *opciones[], int numeroOpciones, int &selec);
+    void Menu_Principal();
+
+
+
+};
+
+#endif // MENU_H
